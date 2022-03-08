22fd09e92d8ce028fc587b9084dde87dac2b5271
Blank <93943699+Blank-sama@users.noreply.github.com>
2022-03-08 12:54:26 +0530

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -1,9 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-"""Userbot module for managing events. One of the main components of the userbot."""
 
 import inspect
 import re
@@ -16,10 +10,10 @@ from traceback import format_exc
 
 from telethon import events
 
-from userbot import CMD_HANDLER, CMD_LIST, DEFAULT, DEVS, bot
+from Bonten import CMD_HANDLER, CMD_LIST, DEFAULT, DEVS, bot
 
 
-def ayiin_cmd(pattern=None, command=None, **args):
+def Bonten_cmd(pattern=None, command=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
     stack = inspect.stack()
     previous_stack_frame = stack[1]
@@ -206,8 +200,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ AYIIN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/AyiinXdSupport)"
+                    text = "**✘ Bonten-Bonten ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/BontenXdSupport)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -216,9 +210,9 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @AyiinXdSupport"
+                    ftext += "\ncukup forward saja pesan ini ke @BontenXdSupport"
                     ftext += "\n================================\n\n"
-                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
+                    ftext += "--------BEGIN Bonten TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
                     ftext += "\nChat ID : " + str(check.chat_id)
                     ftext += "\nUser ID : " + str(check.sender_id)
@@ -228,7 +222,7 @@ def register(**args):
                     ftext += str(format_exc())
                     ftext += "\n\nError text:\n"
                     ftext += str(sys.exc_info()[1])
-                    ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+                    ftext += "\n\n--------END Bonten TRACEBACK LOG--------"
 
                     command = 'git log --pretty=format:"%an: %s" -10'
 
